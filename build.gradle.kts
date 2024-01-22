871e6484c4dee8d61eda94125c3eee2f921b8051
david steinsland <david@davidsteinsland.net>
2024-01-22 14:00:17 +0100

    deployer til dev-gcp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,30 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     kotlin("jvm") version "1.9.22"
 }
 
 val mainClass = "no.nav.helse.spekemat.AppKt"
 val junitJupiterVersion = "5.10.1"
-val jacksonVersion = "2.16.0"
-val logbackClassicVersion = "1.4.14"
-val logbackEncoderVersion = "7.4"
+val rapidsAndRiversVersion = "2024010209171704183456.6d035b91ffb4"
 
 repositories {
+    val githubPassword: String? by project
     mavenCentral()
+    /* ihht. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
+        så plasseres github-maven-repo (med autentisering) før nav-mirror slik at github actions kan anvende førstnevnte.
+        Det er fordi nav-mirroret kjører i Google Cloud og da ville man ellers fått unødvendige utgifter til datatrafikk mellom Google Cloud og GitHub
+     */
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+        credentials {
+            username = "x-access-token"
+            password = githubPassword
+        }
+    }
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
 }
 
 dependencies {
-    api("ch.qos.logback:logback-classic:$logbackClassicVersion")
-    api("net.logstash.logback:logstash-logback-encoder:$logbackEncoderVersion")
-
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    api("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/no/nav/helse/spekemat/App.kt
+++ src/main/kotlin/no/nav/helse/spekemat/App.kt
@@ -1,10 +1,15 @@
 package no.nav.helse.spekemat
 
+import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.LoggerFactory
 
-private val logg = LoggerFactory.getLogger("no.nav.helse.spurte_du.App")
+private val logg = LoggerFactory.getLogger("no.nav.helse.spekemat.App")
 private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
 
 fun main() {
-    println("Hei")
+    RapidApplication.create(System.getenv())
+        .also {
+            logg.info("Hei, er verden klar for pølser?")
+        }
+        .start()
 }
