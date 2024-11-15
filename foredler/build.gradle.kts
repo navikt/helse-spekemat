d2768d8abbd5526caadfdb924399d73ea84a317c
david steinsland <david@davidsteinsland.net>
2024-11-15 09:34:05 +0100

    bumper versjoner

--- foredler/build.gradle.kts
+++ foredler/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
 
-val tbdLibsVersion = "2024.11.14-07.56-63a8564f"
+val tbdLibsVersion = "2024.11.15-09.09-08ca346b"
 val logbackClassicVersion = "1.5.12"
 val logbackEncoderVersion = "8.0"
 val jacksonVersion = "2.18.1"
