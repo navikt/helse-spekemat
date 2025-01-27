a203d09ca1246bc53fd703432b61f8e8983dbe4d
david steinsland <david@davidsteinsland.net>
2025-01-27 13:08:43 +0100

    bumper r&r
    se commit for viktig endring:
    https://github.com/navikt/tbd-libs/commit/17a27722b0843e4c1b3cd16aeada837126b6124b

--- slakter/build.gradle.kts
+++ slakter/build.gradle.kts
@@ -1,4 +1,4 @@
-val rapidsAndRiversVersion = "2024120608421733470968.fc0a3d5e3685"
+val rapidsAndRiversVersion = "2025012712551737978926.de930d8e0feb"
 val tbdLibsVersion = "2024.11.25-10.59-6f263a10"
 val mockkVersion = "1.13.13"
 
