7daeb7ef17373b7c3e85af627e9222073fdca618
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-22 10:06:19 +0200

    Bumper tbd-libs
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- foredler/build.gradle.kts
+++ foredler/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
 
-val tbdLibsVersion = "2025.09.19-13.31-61342e73"
+val tbdLibsVersion = "2025.09.19-15.24-1a9c113f"
 val logbackClassicVersion = "1.5.18"
 val logbackEncoderVersion = "8.0"
 val jacksonVersion = "2.18.3"
