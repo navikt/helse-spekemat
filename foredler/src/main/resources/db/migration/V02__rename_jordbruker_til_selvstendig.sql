73da79290193692a54a4c8ca5e8b8b0ed915367b
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-12-16 13:54:55 +0100

    Sletter jordbruker-pølsepakker der personen allerede har selvstendig-pølsepakker
    Co-authored-by: Sivert Lunsæter <Sivert.Frang.Lunseter@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- foredler/src/main/resources/db/migration/V02__rename_jordbruker_til_selvstendig.sql
+++ foredler/src/main/resources/db/migration/V02__rename_jordbruker_til_selvstendig.sql
@@ -1,3 +1,9 @@
+DELETE
+FROM polsepakke
+WHERE person_id IN (SELECT person_id FROM polsepakke WHERE yrkesaktivitetidentifikator = 'JORDBRUKER')
+  AND person_id IN (SELECT person_id FROM polsepakke WHERE yrkesaktivitetidentifikator = 'SELVSTENDIG')
+  AND yrkesaktivitetidentifikator = 'JORDBRUKER';
+
 UPDATE polsepakke
 SET yrkesaktivitetidentifikator = 'SELVSTENDIG'
 WHERE yrkesaktivitetidentifikator = 'JORDBRUKER';
